0ddded68c29fb47dae802451cc355066861c3113
RushandyAndrea <89991004+RushandyAndrea@users.noreply.github.com>
2021-11-11 10:37:49 -0600

    Update student_maintenance.py
    added few docstrings.
    Fixed update function issue.

--- student_maintenance.py
+++ student_maintenance.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 """
-# Programmer: Rushandy Andrea
-# Date: October 31, 2021
-# Description: This module contains the functions for adding, updating, and deleting student data.
+# Programmer: Rushandy Andrea, Kasey Drevecky
+# Date: November 7th, 2021
+# Description: For this assignment, we had to work as a team to make a Students course and sports
+program that stores list etc.
 """
 
 # Authorship
-__author__ = 'Rushandy Andrea'
+__author__ = 'Rushandy Andrea, Kasey Drevecky'
 __version__ = '1.0'
-__date__ = 'Oct 31, 2021'
+__date__ = 'November 7th, 2021'
 __status__ = 'Development'
 
 import data_validation as val
